68298934abcb2f54c713d1f339af6c798dc2f8e7
Brian Noh <noh24@ymail.com>
2022-10-23 00:15:40 -0400

    WIP: dataset function configuration

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 //select menu 
 const menu = document.getElementById("menu");
 //make an array with menu items
-Array.from(document.getElementsByClassName("menu-items")).forEach(function(item, index) { //can do .forEach((param1, param2) => {code});
-  item.onmouseover = function() { //can do = () => {}, () <- means no parameters => arrow function expression, {} used for multiple line arrow functions
-    menu.dataset.activeIndex = index; //.dataset property allows custom data to be entered, activeIndex = custom attribute
-  }
-});
+Array.from(document.getElementsByClassName("menu-items"))
+  .forEach((item, index) => {
+    item.onmouseover = () => {
+      menu.dataset.activeIndex = index;
+    }
+  });
